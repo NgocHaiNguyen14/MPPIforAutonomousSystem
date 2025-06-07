adeeae65d565e151de1f09de61dc7edf90d1bb76
trankietmashiro <trananhkiet1410@gmail.com>
2025-06-07 10:47:24 -0400

    Update MPPI_Python

--- MPPI_Python/main.py
+++ MPPI_Python/main.py
@@ -20,18 +20,14 @@ output: next state vector
 """
 
 # Cost function definitions
+# Assume all inputs are array
+def runningCost(x, xd, u, du, Q, R, nuy):
+    qx = (x-xd).T @ Q @ (x-xd)
 
-def cost(x, u, Q, R):
-    x_flat = np.array(x)
-    u = np.array(u).reshape(-1)
+    return qx + 1/2*u.T @ R @ u + (1-1/nuy)/2*du.T @ R @ du + u.T @ R @ du
 
-    return x_flat.T @ Q @ x_flat + u.T @ R @ u
-
-def final_cost(x, xd, Qf):
-    x_flat = np.array(x)
-    xd_flat = np.array(xd)
-
-    return (x_flat - xd_flat) @ Qf @ (x_flat - xd_flat).T
+def finalCost(x, xd, Qf):
+    return (x - xd).T @ Qf @ (x - xd)
 
 """
 roll_out: shoot the dynamics
@@ -47,25 +43,13 @@ output: new xtraj ((step + 1) x 12)
 # MPPI: Dynamics & Cost
 
 def roll_out(x0, xtraj, utraj, dt):
-    xtraj[0] = x0
+    xtraj[:,0] = x0
     for i in range(len(utraj)):
         xdot = quadrotor(xtraj[i], utraj[i])
         result = np.array(xtraj[i]) + dt * np.array(xdot)  
         xtraj[i+1] = result.tolist()
 
     return xtraj
-
-
-"""
-Backward pass:
-
-xtraj: trajectory ((step + 1) x 12).
-The first position is initial condition. Last is desired target
-utraj: control input vector (step x 4)
-
-output: new xtraj ((step + 1) x 12)
-
-"""
 
 
 """
@@ -77,75 +61,103 @@ utraj: control inputs (step x 4)
 output: cost of trajectory
 """
 
-def total_cost(xtraj, utraj, xd, Q, R, Qf):
+def total_cost(xtraj, utraj, dutraj, xd, Q, R, Qf, nuy):
     traj_cost = 0
     for i in range(len(utraj)):
-        traj_cost += cost(xtraj[i], utraj[i], Q, R)
-    traj_cost += final_cost(xtraj[-1], xd, Qf)
+        traj_cost += runningCost(xtraj[i], xd, utraj[i], dutraj[i], Q, R, nuy)
+    traj_cost += finalCost(xtraj[-1], xd, Qf)
     return traj_cost
 
 
 # Sampling and Control
 
 """
-Generate sample: randomly generate control inputs
-N: number of samples
-time_step: discretized time step
-nu: number of inputs
-
-output: rollouts of utraj (step x nu x N)
-"""
-
-def gen_sample(N, scale, nu, step):
-    dutraj =  scale * np.random.rand(step, nu, N)
-    
-    return dutraj
-
-"""
 Update u: update inputs based on cost 
 
 HYPER-PARAMETER: LAMDA
 
-input: rollouts of utraj (step x nu x N) and cost(1xN)
+input: utraj (nU x T), rollouts of dutraj (nU x T x N), and cost(N)
 
-output: optimal utraj(step x nu x 1)
+output: optimal utraj(nU x T)
 """
 
-def optimal_u(rutraj, cost):
-    lamda = 10000
-    weights = np.exp(-np.array(cost) / lamda)
-    weights_sum = np.sum(weights)
-    u = (rutraj @ weights) / weights_sum
-    return u
+def optimal_u(utraj, dU, Straj, lamda):
+    
+    for t in range(utraj.shape[1]):
+        ss = 0
+        su = 0
+        minS = min(Straj)
+
+        for k in range(len(Straj)):
+            weight = np.exp(-1.0 / lamda * (Straj[k] - minS))
+            ss += weight
+            su += weight * dU[k][:, t]
+
+        utraj[:,t] += su/ss
+    
+    return utraj
+
+## Initialize new utraj after update
+
+def updateutraj(utraj):
+    nU, time_steps = utraj.shape
+    for i in range(1, time_steps):
+        utraj[:, i-1] = utraj[:, i]
+    utraj[:, -1] = np.zeros(nU)
+
+    return utraj
 
 # Main
+nX = 12 # Number of states
+nU = 4 # Number of inputs
 
-# iLQR params
-Q = np.eye(len(x_flat))
-R = 0*np.eye(len(u))
-
-Qf = np.diag([10000, 10000, 10000,    # e.g. position x, y, z
-   10, 10, 10,          # e.g. velocity x, y, z
-   100, 100, 100,       # e.g. orientation roll, pitch, yaw
-   1, 1, 1])             # e.g. bias or other states
+# Boundary Conditions
+x0 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+xd = np.array([1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
 # MPPI Params
 N = 1000         # Number of MPPI samples
-nu = 4            # 4 ang vel^2 inputs: w1, w2, w3, w4
-scale = 10         # Force scale
+T = 150          # Max number of time steps
+dt = 0.02 # delta time
 
-# Trajectories params
-x0 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+covU = np.diag([2.5,5*1e-3,5*1e-3,5*1e-3])
+lamda = 10
+nuy = 1000
 
-xd = [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# Cost matrices
+Q = np.eye(nX)
+R = lamda*np.linalg.inv(covU)
 
-t = 0.1
-steps = 10        # Number of time steps
-dt = t/steps         # Time step in second
+Qf = np.diag([2.5, 2.5, 10,    # e.g. position x, y, z
+            1, 1, 10,        # e.g. orientation roll, pitch, yaw
+            0, 0, 0,         # e.g. velocity x, y, z
+            0, 0, 0])        # e.g. bias or other states
 
-u_base = iLQR(x0, xd, dt, Q, R, Qf)
-
+utraj = np.zeros((nU, T-1))
 u_opt = []
+xf = []
 
+x = x0
 
+for iter in range(500):
+    xf = xf.append(x)
+    Straj = np.zeros(N)
+    dU = [None] * N
 
+    for k in range(N):
+        dutraj = covU @ np.random.randn(nU, T-1)
+        dU[k] = dutraj
+
+        xtraj = np.zeros((nX, T))
+        xtraj[:,0] = x
+
+        xtraj = roll_out(x, xtraj, utraj+dutraj, dt)
+        Straj[k] = total_cost(xtraj, utraj, dutraj, xd, Q, R, Qf, nuy)
+    
+    utraj = optimal_u(utraj, dU, Straj, lamda)
+
+    # Excecute utraj(0)
+    x += quadrotor(x, utraj[:,0])*dt
+    u_opt = u_opt.append(utraj[:,0])
+
+    utraj = updateutraj(utraj)
